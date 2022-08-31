aa4df53333cbd20b3285e0fb7073f5b80cfb90e0
tillytillie <tillytillie1@gmail.com>
2022-08-31 09:47:31 -0400

    added new features

--- js/script.js
+++ js/script.js
@@ -1,33 +1,114 @@
-/*function add() {
-  var table = document.getElementById('table'),
-    newRow = table.insertRow(table.lenght),
-    cell1 = newRow.insertRow(0),
-    cell2 = newRow.insertRow(1),
-    cell3 = newRow.insertRow(2)
-  fname = document.getElementById('fname').value
-  lname = document.getElementById('lname').value
-  age = document.getElementById('age').value
+'use strict'
+function validateForm(){
+  var myForm = document.getElementById('form')
+  myForm.addEventListener('submit', ()=>{
+    console.log("Signup page clicked")
+    e.preventDefault()
+  })
 
-  cell1.innerHTML = fname
-  cell2.innerHTML = lname
-  cell3.innerHTML = age
-  //cell1 = newRow.insertRow(0),
+  //form validation start here
+  var fname = document.forms['myForm']['fname'].value
+  if (fname == '') {
+    //alert('first name empty')
+    let fnameError = document.getElementById('fnameerror')
+    fnameError.innerHTML = 'Pleas enter a valid First Name'
+    document.forms['myForm']['fname'].focus()
+    //fnameError.style.color = 'gray'
+    return false  
+  }
 
-  //document.getElementById('learning').addEventListener('click', {})
+    //lname validation
+    const lname = document.forms['myForm']['lname'].value
+    if (lname == '') {
+      //alert('Last name cannot be empty')
+      let lnameError = document.getElementById('lnameerror')
+      lnameError.innerHTML = 'Please Provide a valid Last Name'
+      document.forms['myForm']['lname'].focus()
+        return false
+    }
+    //age validation
 
-  document.getElementById('fname').innerHTML = value
-  document.getElementById('lname').innerHTML = value
-  document.getElementById('age').innerHTML = value
+  const phone = document.forms['myForm']['phone'].value
+  if (phone == '') {
+    //alert('phone cannot be empty')
+    let ageerror = document.getElementById('ageerror')
+    ageerror.innerHTML = 'Provde a valid phone'
+    document.forms['myForm']['phone'].focus()
+    return false
+  }
 
-  console.log(fname)
+  const passwordOne = document.forms['myForm']['password1'].value
+  const passwordTwo = document.forms['myForm']['password2'].value
+
+  if (passwordOne.length < 8) {
+    let passwordOneError = document.getElementById('pass1error')
+    passwordOneError.innerHTML = 'Password must be at least 8 character long'
+    document.forms['myForm']['password1'].focus()
+    return false
+  }
+  if (passwordOne == passwordTwo) {
+    return true
+  } else {
+    let passwordTwoError = document.getElementById('pass2error')
+    passwordTwoError.innerHTML = 'Password must be match'
+    document.forms['myForm']['password2'].focus()
+    return false
+  }
 }
 
-let form = document.getElementsByClassName('form')
-form.addEventListerner('click', (e) => {
-  console.log('form is click')
 
-  e.preventDefault()
-})*/
+ //validation for login details 
 
-let date = new Date(Y).getFullyear
-console.log(date)
+ function loginForm(){
+  var login = document.getElementById('loginform')
+  login.addEventListener('submit', () => {
+    e.preventDefault()
+  })
+
+  var username = document.forms['loginForm']['username'].value
+  if (username == "") {
+    let usernameerror = document.getElementById('usernameerror')
+    usernameerror.innerHTML = "Please input a valid user name"
+    document.forms['loginForm']['username'].focus()
+    console.log(username)
+    return false 
+    
+  }
+/*
+  var password = document.forms['loginForm']['password'].value
+  if  (password.length > 8){
+    let passwordError = document.getElementById('passworderror')
+    passwordError.innerHTML = "Password must more than 8 characters"
+    return false
+  }
+  else if(password == ""){
+    password.innerHTML = "Password cannt be empty"
+    return false
+  }*/
+
+}
+//show more
+ //function display(){
+  const displays = document.getElementById('display');
+  const moreoption = document.getElementById('moreoptions')
+  
+  displays.addEventListener('click', () => {
+    if(moreoption === 'none'){
+      moreoption.style.display = 'none'
+    }
+    else{
+      moreoption.style.display = 'block'
+    }
+  })
+  
+  //moreoption.style.color = "red"
+  
+//   displays.addEventListener('click', ()=>{
+//       if(moreoption === 'block'){
+//         moreoption.style.display = 'none'
+//       }
+//       else{
+//         moreoption.style.display = 'none'
+//       }
+//     })  
+//  }
